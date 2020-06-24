68b59ffd12459afe4b421af6acdd580d3dafd372
Jairaj <67155126+jairaj09@users.noreply.github.com>
2020-06-24 09:44:51 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a , b , c , d ;
+    printf("the values of a,b and c is ");
+    scanf("%d %d %d" ,&a,&b,&c);
+    d = a+b+c ;
+    printf("the sum of %d , %d and %d is %d", a,b,c,d);
+    return 0 ;
+}
